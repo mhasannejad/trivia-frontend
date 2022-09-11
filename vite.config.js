e044124cb04db2b6b36db7431589fb1ed8e981f5
drgeek <mngmnt.hasannejad@gmail.com>
2022-09-11 22:15:37 +0430

    fix routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: { exclude: ["svelte-navigator"] },
+    optimizeDeps: {exclude: ["svelte-navigator"]},
+    plugins: [svelte()],
+
 })
